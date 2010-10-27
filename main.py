424bbfc56d9e8d301e483f20da4ca6b16b904a39
Vincent Mi <vinc3m1@gmail.com>
2010-10-27 00:55:39 -0500

    styled the /stay page. blogger pictures not working because hotlinking is disabled

--- main.py
+++ main.py
@@ -51,13 +51,20 @@ class StayPage(webapp.RequestHandler):
     if entries is None:
       entries = self.get_cached_entries()
       memcache.add("entries", entries, 43200)
+    elif len(entries[0]) != 5:
+      memcache.flush_all()
+      entries = self.get_cached_entries()
+      memcache.add("entries", entries, 43200)
     
     entry = entries[random.randint(0,len(entries)-1)]
+    # entry = entries[506]
     
     template_values = {
-                'imgurl': entry[0],
-                'content': entry[1],
-                'date': entry[2]
+                'title' : entry[0],
+                'date': entry[1],
+                'imgurl': entry[2],
+                'content': entry[3],
+                'url': entry[4]
                 }
 
     path = os.path.join(os.path.dirname(__file__), 'index.html')
@@ -83,26 +90,16 @@ class StayPage(webapp.RequestHandler):
 
     cachedentries = []
     for entry in entries:
+      title = entry.title.text
       imgurl = re.findall('href="([^"]*)"', entry.content.text)[0]
-      content = re.sub('<br[ ]*/>', '\n', entry.content.text)
-      content = re.sub('<.*?>', ' ', content)
+      content = re.sub('<!--.*?-->', '', entry.content.text)
+      content = re.sub('<br[ ]*/>', '\n', content)
+      content = re.sub('<.*?>|', '', content)
       content = string.strip(content)
-      # content = re.findall('</a>[<br />|<div>|</div>]*(.*)</div', entry.content.text)
-      date = datetime.datetime.strptime(entry.published.text[:10], "%Y-%m-%d").strftime("%B %d %Y")
-      # if imgurl:
-      #   imgurl = imgurl[0]
-      # else:
-      #   imgurl = ""
-      # if content:
-      #   # content = re.findall('(.*)</div>', content[0])
-      #   # if content:
-      #     content = content[0]
-      #   # else:
-      #   #      content = ""
-      # else:
-      #   content = ""
-      
-      cachedentry = [imgurl, content, date]
+      content = re.sub('\n', '<br />', content)
+      date = datetime.datetime.strptime(entry.published.text[:10], "%Y-%m-%d").strftime("%A, %B %d, %Y")
+      url = entry.link[-1].href
+      cachedentry = [title, date, imgurl, content, url]
       cachedentries.append(cachedentry)
 
     return cachedentries
