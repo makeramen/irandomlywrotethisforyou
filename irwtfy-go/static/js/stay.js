54493570027e3ff0e11e49f63575cffac1124ce3
Vince Mi <vinc3m1@gmail.com>
2018-06-09 18:40:00 -0700

    make shared.js and bri.js

--- irwtfy-go/static/js/stay.js
+++ irwtfy-go/static/js/stay.js
@@ -23,7 +23,7 @@ function getCount() {
         Cookies.set('count', count, { expires: 365 })
         return Promise.resolve(count)
     })
-    }
+}
 
 function getRandomEntry() {
     return getCount()
@@ -46,70 +46,7 @@ function getRandomEntry() {
             $app.published = new Date(entry.published.$t).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
             $app.title = entry.title.$t
             $app.url = entry.link.find(function(l) { return l.rel == 'alternate' }).href
-            $app.content = entry.content.$t
-                .replace(/(?:(?:<div>)?\s*<br\s*\/?>\s*(?:<\/div>)?\s*){3,}/gi, '')
-                .replace(/http:\/\//gi, 'https://')
-            Vue.nextTick(function() {
-                // Set all anchors that wrap images to display: block
-                $("#content a:has(img)").css("display","block");
-
-                // Remove any line breaks immediately before the text
-                var node = $("#content").contents().toArray()
-                    .find(function(n) { return n.nodeType == 3 && $.trim(n.textContent) })
-                while (node && node.previousSibling
-                    && (node.previousSibling.nodeName == "BR" || node.previousSibling.nodeType == 8)) {
-                    node.previousSibling.remove();
-                }
-
-                // Find all YouTube and Vimeo videos
-                $allVideos = $("iframe[src*='www.youtube.com'], iframe[src*='player.vimeo.com']");
-
-                if ($allVideos.length > 0) {
-                    // Figure out and save aspect ratio for each video
-                    $allVideos.each(function() {
-                        $(this)
-                        .data('aspectRatio', this.height / this.width)
-                        // and remove the hard coded width/height
-                        .removeAttr('height')
-                        .removeAttr('width');
-                    });
-
-                    // Kick off one resize to fix all videos on page load
-                    $(window).resize()
-                }
-            })
+            $app.content = sanitizeContent(entry.content.$t)
+            Vue.nextTick(cleanUpAfterLoad)
         })
 }
-
-var $app = new Vue({
-    el: "#wrapper",
-    data : {
-        published: '',
-        title: '',
-        url: '',
-        content: '',
-    }
-})
-var $ptr = PullToRefresh.init({
-    mainElement: '#wrapper',
-    passive: true,
-    onRefresh: function(done) { 
-        getRandomEntry().then(done)
-    }
-})
-var $allVideos
-getRandomEntry(function() {})
-
-// When the window is resized
-$(window).resize(function() {
-    if (!$allVideos) { return }
-    // Resize all videos according to their own aspect ratio
-    $allVideos.each(function() {
-    var $el = $(this);
-    // Get parent width of this video
-    var newWidth = $el.parent().width();
-    $el
-        .width(newWidth)
-        .height(newWidth * $el.data('aspectRatio'));
-    });
-});
