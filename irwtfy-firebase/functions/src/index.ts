c3ea7e211bf6531fc0b6b21e507b38dd7632a2be
Vince Mi <vinc3m1@gmail.com>
2018-06-10 22:54:41 -0700

    firebase

--- irwtfy-firebase/functions/src/index.ts
+++ irwtfy-firebase/functions/src/index.ts
@@ -1,3 +1,5 @@
+import * as https from 'https'
+import axios from 'axios'
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 
@@ -5,7 +7,46 @@ admin.initializeApp(functions.config().firebase)
 
 const db = admin.firestore()
 const countRef = db.collection('irwtfy').doc('count')
+const agent = new https.Agent({ keepAlive: true })
 
-export const randomEntry = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+export const randomEntry = functions.https.onRequest((request, response) => countRef
+  .get()
+  .then(doc => {
+    if (!doc.exists) {
+      return Promise.reject('document doesnt exist')
+    } else {
+      return Promise.resolve(doc.data().count)
+    }
+  })
+  .catch(err => axios
+    .get('https://www.blogger.com/feeds/6752139154038265086/posts/default', {
+      params: {
+        'alt': 'json',
+        'start-index': 1,
+        'max-results': 1,
+      },
+      httpsAgent: agent,
+    })
+    .then(resp => {
+      const c = resp.data.feed.openSearch$totalResults.$t
+      return countRef.set({ count: c }).then(_ => Promise.resolve(c))
+    }))
+  .then(count => axios
+    .get('https://www.blogger.com/feeds/6752139154038265086/posts/default', {
+      params: {
+        'alt': 'json',
+        'start-index': Math.floor(Math.random() * count) + 1,
+        'max-results': 1,
+      },
+      httpsAgent: agent,
+    })
+    .then(resp => {
+      const url = resp.data.feed.entry[0].link.find(l => l.rel === 'alternate').href
+      const c = resp.data.feed.openSearch$totalResults.$t
+      if (c !== count) {
+        return countRef.set({ count: c }).then(_ => Promise.resolve(url))
+      }
+      return Promise.resolve(url)
+    })
+    .then(url => response.redirect(307, url))
+  ));
