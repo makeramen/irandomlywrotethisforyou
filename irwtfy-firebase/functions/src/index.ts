97739610b3ddbf314012eb62ac59e478ca829dab
Vince Mi <vinc3m1@gmail.com>
2018-06-11 19:48:35 -0700

    use async function and lts node

--- irwtfy-firebase/functions/src/index.ts
+++ irwtfy-firebase/functions/src/index.ts
@@ -1,6 +1,6 @@
 import { Agent } from 'https'
 import axios from 'axios'
-import { initializeApp, firestore} from 'firebase-admin'
+import { initializeApp, firestore } from 'firebase-admin'
 import { config, https } from 'firebase-functions'
 
 initializeApp(config().firebase)
@@ -9,21 +9,14 @@ const db = firestore()
 const countRef = db.collection('irwtfy').doc('count')
 const agent = new Agent({ keepAlive: true })
 
-export const randomEntry = https.onRequest((request, response) => countRef.get()
-  .then(doc => {
-    if (doc.exists && 'count' in doc.data()) {
-      const count = parseInt(doc.data().count)
-      if (typeof count === 'number' && count % 1 === 0) {
-        return Promise.resolve(doc.data().count)
-      }
-      console.log('Store count is bad format!')
-      return Promise.reject(undefined)
-    }
-    console.log('No stored count!')
-    return Promise.reject(undefined)
-  })
-  .catch(err => axios
-    .get('https://www.blogger.com/feeds/6752139154038265086/posts/default', {
+export const randomEntry = https.onRequest(async (request, response) => {
+  const doc = await countRef.get()
+  const data = doc.data()
+  let count
+  if (data && data.count && parseInt(data.count) % 1 === 0) {
+    count = parseInt(data.count)
+  } else {
+    const countResponse = await axios.get('https://www.blogger.com/feeds/6752139154038265086/posts/default', {
       params: {
         'alt': 'json',
         'start-index': 1,
@@ -31,26 +24,24 @@ export const randomEntry = https.onRequest((request, response) => countRef.get()
       },
       httpsAgent: agent,
     })
-    .then(resp => {
-      const c = resp.data.feed.openSearch$totalResults.$t
-      return countRef.set({ count: c }).then(_ => Promise.resolve(c))
-    }))
-  .then(count => axios
-    .get('https://www.blogger.com/feeds/6752139154038265086/posts/default', {
-      params: {
-        'alt': 'json',
-        'start-index': Math.floor(Math.random() * count) + 1,
-        'max-results': 1,
-      },
-      httpsAgent: agent,
-    })
-    .then(resp => {
-      const url = resp.data.feed.entry[0].link.find(l => l.rel === 'alternate').href
-      const c = parseInt(resp.data.feed.openSearch$totalResults.$t)
-      if (c !== count) {
-        return countRef.set({ count: c }).then(_ => Promise.resolve(url))
-      }
-      return Promise.resolve(url)
-    })
-    .then(url => response.redirect(307, url))
-  ));
+    count = countResponse.data.feed.openSearch$totalResults.$t
+    await countRef.set({ count: count })
+  }
+
+  const resp = await axios.get('https://www.blogger.com/feeds/6752139154038265086/posts/default', {
+    params: {
+      'alt': 'json',
+      'start-index': Math.floor(Math.random() * count) + 1,
+      'max-results': 1,
+    },
+    httpsAgent: agent,
+  })
+  const url = resp.data.feed.entry[0].link.find(l => l.rel === 'alternate').href
+  const c = parseInt(resp.data.feed.openSearch$totalResults.$t)
+
+  if (c !== count) {
+    await countRef.set({ count: c })
+  }
+
+  response.redirect(307, url)
+})
