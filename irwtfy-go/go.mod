8e3690d52b12c5330341f4e0fdb9e254009da4fa
Vince Mi <vinc3m1@gmail.com>
2019-07-16 04:07:02 -0700

    upgrade to go 1.12

---
+++ irwtfy-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/vincemi/irandomlywrotethisforyou/irwtfy-go
+
+go 1.12
--- irwtfy-go/main.go
+++ irwtfy-go/main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"context"
 	json "encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
-
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/log"
-	"google.golang.org/appengine/memcache"
-	"google.golang.org/appengine/urlfetch"
 )
 
 const blogID string = "6752139154038265086"
@@ -23,32 +19,62 @@ const keyNumPosts = "num_posts"
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	http.HandleFunc("/", handleRedirect)
-	appengine.Main()
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	// [END setting_port]
 }
 
 func handleRedirect(w http.ResponseWriter, r *http.Request) {
-	ctx := appengine.NewContext(r)
+	count := getCookieCount(r)
 
-	client := urlfetch.Client(ctx)
-
-	postURL, _, _, err := getRandomPost(ctx, client)
+	postURL, _, count, err := getRandomPost(count)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	newCookie := http.Cookie{
+		Name:   "count",
+		Value:  strconv.Itoa(count),
+		MaxAge: 365 * 24 * 60 * 60 * 1000,
+	}
+	http.SetCookie(w, &newCookie)
+
 	// redirect to a random url
 	http.Redirect(w, r, postURL, 302)
 }
 
-func getRandomPost(ctx context.Context, client *http.Client) (url string, post entry, count int, err error) {
-	postCount, err := getPostCount(ctx, client)
+func getCookieCount(r *http.Request) (count int) {
+	count = -1
+	currentCookie, err := r.Cookie("count")
 	if err != nil {
 		return
 	}
+	cookieCount, err := strconv.Atoi(currentCookie.Value)
+	if err != nil {
+		return
+	}
+	count = cookieCount
+	return
+}
+
+func getRandomPost(postCount int) (url string, post entry, count int, err error) {
+	if postCount < 0 {
+		_, _, postCount, err = getPost(1, 0)
+		if err != nil {
+			return
+		}
+	}
 
 	for url == "" {
-		url, post, count, err = getPost(ctx, client, rand.Intn(postCount)+1, postCount)
+		url, post, count, err = getPost(rand.Intn(postCount)+1, postCount)
 		if err != nil {
 			return
 		}
@@ -56,13 +82,13 @@ func getRandomPost(ctx context.Context, client *http.Client) (url string, post e
 	return
 }
 
-func getPost(ctx context.Context, client *http.Client, index int, prevCount int) (url string, post entry, count int, err error) {
+func getPost(index int, prevCount int) (url string, post entry, count int, err error) {
 	count = 0
 	post = entry{}
 	url = ""
 	request := fmt.Sprintf("https://www.blogger.com/feeds/%s/posts/default?alt=json&start-index=%d&max-results=1", blogID, index)
-	log.Debugf(ctx, "request: %s", request)
-	resp, err := client.Get(request)
+	log.Printf("request: %s", request)
+	resp, err := http.Get(request)
 	if err != nil {
 		return
 	}
@@ -86,26 +112,5 @@ func getPost(ctx context.Context, client *http.Client, index int, prevCount int)
 		}
 	}
 
-	if count != prevCount {
-		item := &memcache.Item{
-			Key:        keyNumPosts,
-			Value:      []byte(strconv.Itoa(count)),
-			Expiration: 12 * time.Hour,
-		}
-		err = memcache.Set(ctx, item)
-	}
-
-	return
-}
-
-func getPostCount(ctx context.Context, client *http.Client) (count int, err error) {
-	item, err := memcache.Get(ctx, keyNumPosts)
-	if err == memcache.ErrCacheMiss {
-		_, _, count, err = getPost(ctx, client, 1, 0)
-		return
-	} else if err != nil {
-		return
-	}
-	count, err = strconv.Atoi(string(item.Value[:]))
 	return
 }
